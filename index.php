100255797b943497333647d6ff4260e12d945c65
Martin <tanomartin05@gmail.com>
2015-06-05 19:01:29 -0300

    Delete de las imagenes por defecto. Alta de proximamente

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<p>PROXIMAMENTE</p>
-<p>LA VECCHIA FUTBOL </p>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <style>
+    html {
+        height:100%;
+        width:100%;
+    }
+    body{
+        background:#8ba987 url('img/proximamente.jpg') no-repeat center center;
+        background-size:100% 100%;
+    }
+    </style>
+</head>
+<body>
+</body>
+</html>
